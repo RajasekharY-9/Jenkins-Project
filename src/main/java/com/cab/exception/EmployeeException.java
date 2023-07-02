df4da8a7afca60e8777cc04e05787e7c553ce907
RajasekharY <yalamillirajasekhar123@gmail.com>
2023-07-02 16:56:30 +0530

    CRUD

---
+++ src/main/java/com/cab/exception/EmployeeException.java
@@ -0,0 +1,17 @@
+package com.cab.exception;
+
+public class EmployeeException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeException(String message) {
+		super(message);
+		
+	}
+
+	
+	
+}
